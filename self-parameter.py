87942395011fd293930eb5a016ac462f64ac10f6
jaiganesh2108 <jaig7335@gmail.com>
2026-03-27 20:47:48 +0530

    merge1

--- self-parameter.py
+++ self-parameter.py
@@ -7,3 +7,13 @@ exc = Example()
 print(exc.a)
 exc.add(20)
 print(exc.a)
+
+
+class Car:
+    def __init__(self, brand):
+        self.brand = brand
+    def show(self):
+        print(self.brand)
+
+c1 = Car("Ford")
+c1.show()
