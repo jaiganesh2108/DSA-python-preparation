0fc93c244178fd383e1af5a97399082e13f72a56
jaiganesh2108 <jaig7335@gmail.com>
2026-03-27 22:35:53 +0530

    final merge

--- self-parameter.py
+++ self-parameter.py
@@ -7,6 +7,7 @@ exc = Example()
 print(exc.a)
 exc.add(20)
 print(exc.a)
+print(Example.a) #class variable a is still 10, instance variable a is 20
 
 """ Instructions
 Inside the editor, complete the following steps:
@@ -15,7 +16,6 @@ Add an __init__ method with a brand parameter, and store it as a property
 Add a method called show that prints the brand
 Create an object c1 of the Car class with brand "Ford"
 Call the show method on c1"""
-
 class Car:
     def __init__(self, brand):
         self.brand = brand
